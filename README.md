92c7d9aa30a4820cb2f1106e3b93078e018c117f
Grace KAGAJU <kagajugrace@gmail.com>
2023-07-23 22:55:37 +0200

    notebooks

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# LP5_SENTIMENT-ANALYSI-WITH-NLP
+# SENTIMENT-ANALYSI-WITH-NLP AND HUGGING FACE
+
+## Overview
+- This project was introduction to sentiment analysis with Natural Language Processing
+
+## Background
+- This project gave me the opportunity to fine-tune a pre-trained Deep Learning models from HuggingFace on a new dataset to modify models to predict the sentiments from twitter display neutral, positive, negative for the end-user. A streamlit app was then created and models trained was integrated to deploy the app on the HuggingFace platform.
+
+The streamlit app end-user displays or expressed an input text and displays a visual representation of the sentiment scores.
+
+## Hugging Face
+
+
+## Medium Article
+
+
+# Author 
+Marie Grace KAGAJU
---
+++ notebooks/one.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import transformers
+import torch
+
+# Load the model and tokenizer
+model = transformers.AutoModelForSequenceClassification.from_pretrained("RICHARDMENSAH/twitter_xlm_roberta_base")
+tokenizer = transformers.AutoTokenizer.from_pretrained("RICHARDMENSAH/twitter_xlm_roberta_base")
+
+# Define the function for sentiment analysis
+@st.cache_resource
+def predict_sentiment(text):
+    # Tokenize the input text
+    inputs = tokenizer(text, return_tensors="pt")
+    # Pass the tokenized input through the model
+    outputs = model(**inputs)
+    # Get the predicted class and return the corresponding sentiment
+    predicted_class = torch.argmax(outputs.logits, dim=-1).item()
+    if predicted_class == 0:
+        return "Negative"
+    elif predicted_class == 1:
+        return "Neutral"
+    else:
+        return "Positive"
+
+# Setting the page configurations
+st.set_page_config(
+    page_title="Sentiment Analysis App",
+    page_icon=":smile:",
+    layout="wide",
+    initial_sidebar_state="auto",
+)
+
+# Add description and title
+st.write("""
+# How Positive or Negative is your Text?
+Enter some text and we'll tell you if it has a positive, negative, or neutral sentiment!
+""")
+
+
+# Add image
+image = st.image("https://i0.wp.com/thedatascientist.com/wp-content/uploads/2018/10/sentiment-analysis.png", width=400)
+
+# Get user input
+text = st.text_input("Enter some text here:")
+
+# Define the CSS style for the app
+st.markdown(
+"""
+<style>
+body {
+    background-color: #f5f5f5;
+}
+h1 {
+    color: #4e79a7;
+}
+</style>
+
+""",
+unsafe_allow_html=True
+)
+
+
+# Show sentiment output
+if text:
+    sentiment = predict_sentiment(text)
+    if sentiment == "Positive":
+        st.success(f"The sentiment is {sentiment}!")
+    elif sentiment == "Negative":
+        st.error(f"The sentiment is {sentiment}.")
+    else:
+        st.warning(f"The sentiment is {sentiment}.")
